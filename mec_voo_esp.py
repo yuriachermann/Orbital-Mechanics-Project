cc7a11e72eb03fa18e2b38c15ec06ae6acc6c62f
luizffxv <76705825+luizffxv@users.noreply.github.com>
2021-04-29 20:38:49 -0300

    Update mec_voo_esp.py

--- mec_voo_esp.py
+++ mec_voo_esp.py
@@ -1,1 +1,30 @@
+# Trabalho de Manobras de Mec Voo Espcial
 
+import numpy as np
+from np import cos, sen
+
+
+#Estudo de manobra de mudança de plano
+# - inserção em órbita geocêntrica tem inclinição relativa ao plano do equador
+# - mudar o plano orbital eclípitca
+ 
+#Transferência de Hohmann desde a Terra até (Venus)
+#Alternativamente, não-Hohmann com linha dos apses comum.
+
+#Mudança de fase
+
+#Estudo de um fly-by com Venus
+
+#Especificação da órbita original
+#  a0,e0,I0,Omg0,omg0,tau   (f0,t0)
+  
+#  determinar o ponto A, e tA
+#  qual deve ser o Delta vA
+  
+#  a1,e1,I1,...   (fA,tA)
+#  determinar o ponto C, e tC
+#  Delta vC
+  
+#https://solarsystem.nasa.gov/system/resources/detail_files/11776_Cassini_interplanet_trajectory.png
+
+#https://solarsystem.nasa.gov/resources/11776/cassini-trajectory/
