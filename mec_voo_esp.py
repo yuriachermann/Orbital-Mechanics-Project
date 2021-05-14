03ec7518bc70b352c51248d2d5eea1ef00dba52f
luizffxv <76705825+luizffxv@users.noreply.github.com>
2021-05-14 16:14:47 -0300

    Update mec_voo_esp.py

--- mec_voo_esp.py
+++ mec_voo_esp.py
@@ -1,30 +1,92 @@
-# Trabalho de Manobras de Mec Voo Espcial
+# Code developed by Luiz Junior in 05/12/2021 at 11:18
 
+from math import sqrt
 import numpy as np
-from np import cos, sin
+import sympy as s
 
+# Constants:
+mu_sol = (1.3271)*(10**11) # Em km^3/s^2
 
-#Estudo de manobra de mudança de plano
-# - inserção em órbita geocêntrica tem inclinição relativa ao plano do equador
-# - mudar o plano orbital eclípitca
- 
-#Transferência de Hohmann desde a Terra até (Venus)
-#Alternativamente, não-Hohmann com linha dos apses comum.
+mu_venus = 324900 # Em km^3/s^2
 
-#Mudança de fase
+R_terra = (149.6)*(10**6) # Em km
 
-#Estudo de um fly-by com Venus
+R_venus = (108.2)*(10**6) # Em km
 
-#Especificação da órbita original
-#  a0,e0,I0,Omg0,omg0,tau   (f0,t0)
-  
-#  determinar o ponto A, e tA
-#  qual deve ser o Delta vA
-  
-#  a1,e1,I1,...   (fA,tA)
-#  determinar o ponto C, e tC
-#  Delta vC
-  
-#https://solarsystem.nasa.gov/system/resources/detail_files/11776_Cassini_interplanet_trajectory.png
+r_venus = 6052 # Em km
 
-#https://solarsystem.nasa.gov/resources/11776/cassini-trajectory/
+# Fase 1 da missao:
+
+degress = 30*np.pi/180
+
+e1 = (R_terra-R_venus)/(R_terra+R_venus*np.cos(degress))
+
+h1 = np.sqrt(mu_sol*R_terra*(1-e1)) # Em km^2/s
+
+V_par = h1/R_venus # Em km/s
+
+V_r = (mu_sol/h1)*e1*np.sin(degress) # Em km/s
+
+#print(V_par)
+#print(V_r)
+
+gamma = (np.arctan(V_r/V_par))*180/np.pi 
+
+#print(gamma)
+
+v1v = np.sqrt(V_r**2+V_par**2) # Em km/s
+
+#print(v1v)
+
+# Fase 2 da missao (Flyby para venus):
+
+V1 = np.array((V_par,V_r))
+
+# The velocity of Venus in its presumed circular orbit around the sun is
+
+V_venus = np.sqrt(mu_sol/R_venus)
+
+# print(V_venus) # ok
+
+V = np.array((V_venus,0))
+
+V_inf = V1-V
+
+# disto temos que
+
+v_inf = np.sqrt(V_inf[0]*V_inf[0]+V_inf[1]*V_inf[1])
+
+#print(v_inf) ok
+
+rp = r_venus+300
+
+h_v = rp*np.sqrt(v_inf**2+(2*mu_venus/rp))
+
+e_v = 1 + (rp*(v_inf**2))/mu_venus
+
+deltinha = 2*s.asin(1/e_v)*180/np.pi
+
+theta_inf = s.acos(-1/e_v)*180/np.pi
+
+delta = rp*np.sqrt((e_v+1)/(e_v-1)) # Em km
+
+# angle between v_inf e V
+
+fi = s.atan(V_inf[1]/V_inf[0])*180/np.pi
+
+#print(fi)
+
+fi2_deg = (fi + deltinha)
+
+fi2 = (fi + deltinha)*np.pi/180
+
+V_inf2 = v_inf*np.array((s.cos(fi2),s.sin(fi2)))
+
+V2 = V + V_inf2
+
+V_par2 = V2[0]
+V_r2 = V2[1]
+
+#The speed of the spacecraft at the outbound crossing is
+v2 = sqrt(V_par2**2+V_r2**2) #km/s
+
